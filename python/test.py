7820d65cf2803e6f1c887145d72346f7fe0ab8cc
violet-isle <violet.ravenholm@gmail.com>
2025-02-19 18:28:42 -0500

    init2

--- python/test.py
+++
@@ -1,4 +0,0 @@
-import sys, json, struct
-message = json.dumps({"text": "Test message"}).encode("utf-8")
-sys.stdout.buffer.write(struct.pack("I", len(message)) + message)
-sys.stdout.buffer.flush()
